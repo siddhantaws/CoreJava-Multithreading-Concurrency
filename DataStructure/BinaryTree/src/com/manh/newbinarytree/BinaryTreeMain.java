9fabffc2b0d7d27a46bf11c81d996a7a8b078a9e
siddhantaws <siddhantaws@gmail.com>
2016-03-01 09:47:51 +0530

    Core Java ,MultiThreading and Concurrency

---
+++ DataStructure/BinaryTree/src/com/manh/newbinarytree/BinaryTreeMain.java
@@ -0,0 +1,25 @@
+package com.manh.newbinarytree;
+
+public class BinaryTreeMain
+{
+	public static void main(String[] args) 
+	{
+		//System.out.println(1589415215 & 7);
+		BinaryTree<Integer> binaryTree=new BinaryTree<>();
+		binaryTree.add(50);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		binaryTree.postOrderTraversal();
+		//binaryTree.insert("D");
+		//System.out.println(binaryTree.count());
+		//System.out.println(binaryTree.getMax());
+		//System.out.println(binaryTree.search("E"));
+		//System.out.println(binaryTree.height());
+		//binaryTree.levelOrderTraversal();
+		//System.out.println(binaryTree.getHalfNode());
+		//binaryTree.printAncestor("E");
+		//System.out.println(binaryTree.sumOfBinaryTree());
+		//System.out.println(1494094604 + 721725057 + 1045394743 +1385729494 );
+		//System.out.println(1589415215 & 7);
+	}
+}
